314b5b5b06e83563ce80d532a9e7a9b9c07089cd
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-06-10 18:14:39 +0500

    number of holders, total and over time

---
+++ src/compound/asset-analysis/number-of-holders/total-number-of-holders.sql
@@ -0,0 +1,35 @@
+WITH holdings AS (
+    SELECT
+        DISTINCT address,
+        holding,
+        {{token_address}} AS token_address
+    FROM (
+        SELECT
+            SUM(total) AS holding,
+            address
+        FROM (
+            SELECT 
+                SUM(CAST(value ASevt_block_time DOUBLE) / POW(10, b.decimals)) AS total,
+                to AS address
+            FROM erc20_{{chain}}.evt_Transfer a
+            JOIN tokens.erc20 b ON a.contract_address = b.contract_address
+            WHERE a.contract_address = {{token_address}}
+            GROUP BY 2
+
+            UNION ALL
+
+            SELECT
+                -SUM(CAST(value AS DOUBLE) / POW(10, b.decimals)) AS total,
+                "from" AS address
+            FROM erc20_{{chain}}.evt_Transfer a
+            JOIN tokens.erc20 b ON a.contract_address = b.contract_address
+            WHERE a.contract_address = {{token_address}}
+            GROUP BY 2
+        ) t
+        GROUP BY address
+    ) t
+    WHERE holding >= 1
+)
+
+SELECT COUNT(DISTINCT address) AS total_number_of_holders
+FROM holdings;
