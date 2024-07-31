a6f31986edd19fd6b083947786c8120ea95fd8d5
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-07-31 21:19:50 +0500

    changes in total number of holders

--- src/compound/asset-analysis/15_number-of-holders/total-number-of-holders.sql
+++ src/compound/asset-analysis/15_number-of-holders/total-number-of-holders.sql
@@ -1,96 +1,54 @@
 WITH
   transfers AS (
     SELECT
-      DAY,
-      address,
-      token_address,
-      SUM(amount) AS amount
+      evt_block_time,
+      contract_address,
+      "from",
+      to,
+      value
     FROM
-      (
-        SELECT
-          DATE_TRUNC('day', evt_block_time) AS DAY,
-          "to" AS address,
-          tr.contract_address AS token_address,
-          CAST(value AS DECIMAL (38, 0)) AS amount
-        FROM
-          erc20_{{chain}}.evt_Transfer AS tr
-        WHERE
-          contract_address = {{token_address}}
-        UNION ALL
-        SELECT
-          DATE_TRUNC('day', evt_block_time) AS DAY,
-          "from" AS address,
-          tr.contract_address AS token_address,
-          (-1) * (CAST(value AS DECIMAL (38, 0))) AS amount
-        FROM
-          erc20_{{chain}}.evt_Transfer AS tr
-        WHERE
-          contract_address = {{token_address}}
-      ) AS t
+      erc20_{{chain}}.evt_Transfer
+    WHERE
+      contract_address = {{token_address}}
+      AND "from" <> to
+  ),
+  -- Calculate the balance for each address
+  balances AS (
+    SELECT
+      contract_address,
+      to AS address,
+      SUM(value) AS balance
+    FROM
+      transfers
     GROUP BY
-      1,
-      2,
-      3
-  ),
-  balances_with_gap_days AS (
+      contract_address,
+      to
+    UNION ALL
     SELECT
-      t.day,
-      address,
-      SUM(amount) OVER (
-        PARTITION BY
-          address
-        ORDER BY
-          t.day
-      ) AS balance,
-      LEAD(DAY, 1, CURRENT_TIMESTAMP) OVER (
-        PARTITION BY
-          address
-        ORDER BY
-          t.day
-      ) AS next_day
+      contract_address,
+      "from" AS address,
+      - SUM(value) AS balance
     FROM
-      transfers AS t
-  ),
-  days AS (
-    SELECT
-      DAY
-    FROM
-      UNNEST (
-        SEQUENCE(
-          TRY_CAST(
-            DATE_TRUNC('day', CURRENT_TIMESTAMP) AS TIMESTAMP
-          ),
-          CAST(
-            TRY_CAST(
-              TRY_CAST(
-                TRY_CAST(DATE_TRUNC('day', CURRENT_TIMESTAMP) AS TIMESTAMP) AS TIMESTAMP
-              ) AS TIMESTAMP
-            ) AS TIMESTAMP
-          ),
-          INTERVAL '1' day
-        )
-      ) AS _u (DAY)
-  ),
-  balance_all_days AS (
-    SELECT
-      d.day,
-      address,
-      SUM(balance / TRY_CAST(POWER(10, 0) AS DOUBLE)) AS balance
-    FROM
-      balances_with_gap_days AS b
-      INNER JOIN days AS d ON b.day <= d.day
-      AND d.day < b.next_day
+      transfers
     GROUP BY
-      1,
-      2
-    ORDER BY
-      1,
-      2
+      contract_address,
+      "from"
+  ),
+  -- Aggregate the balances to get the final balance for each address
+  final_balances AS (
+    SELECT
+      contract_address,
+      address,
+      SUM(balance) AS balance
+    FROM
+      balances
+    GROUP BY
+      contract_address,
+      address
   )
 SELECT
-  COUNT(address) AS "Holders"
+  COUNT(*) AS distinct_holders
 FROM
-  balance_all_days AS b
+  final_balances
 WHERE
   balance > 0
-
